6143b782335ea2fe9a88ae419f43117c3c4e5bf4
criskell <96352451+criskell@users.noreply.github.com>
2023-08-31 10:40:27 -0300

    feat: add donation list page (#13)

---
+++ resources/views/donation/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layout.app')
+
+@section('title', 'List of donations')
+
+@section('content')
+    <a class="btn-link btn-primary" href="{{ route('donation.create') }}">Criar doação</a>
+
+    <table class="table">
+        <thead>
+            <th scope="col">Correlation ID</th>
+            <th scope="col">Status</th>
+            <th scope="col">Value</th>
+            <th scope="col">Comment</th>
+            <th scope="col">Actions</th>
+        </thead>
+        <tbody>
+            @foreach ($donations as $donation)
+                <tr>
+                    <th scope="row">{{ $donation->correlationID }}</th>
+                    <td>{{ $donation->status }}</td>
+                    <td>{{ $donation->value }}</td>
+                    <td>{{ $donation->comment }}</td>
+                    <td>
+                        <a href="{{ route('donation.show', [$donation]) }}">
+                            View donation
+                        </a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
